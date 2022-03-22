245b0b1e01e2b014f0e01ed2768d650668ed98e2
WINNIC <62343653+wangsicong120@users.noreply.github.com>
2022-03-22 22:01:01 +0800

    Update ubuntu_crack.sh

--- ubuntu_crack.sh
+++ ubuntu_crack.sh
@@ -6,9 +6,9 @@ rm -f /opt/emby-server/system/MediaBrowser.Model.dll
 wget -P /opt/emby-server/system/ https://github.com/wangsicong120/Emby/raw/main/MediaBrowser.Model.dll
 echo "核心破解完成..."
 rm -f /opt/emby-server/system/dashboard-ui/bower_components/emby-apiclient/connectionmanager.js
-wget -P /opt/emby-server/system/dashboard-ui/bower_components/emby-apiclient/ https://raw.githubusercontent.com/wangsicong120/Emby/main/connectionmanager.js?token=GHSAT0AAAAAABSWSGH2MWU5X64R3ZD7EBZIYRZZ2NA
+wget -P /opt/emby-server/system/dashboard-ui/bower_components/emby-apiclient/ https://raw.githubusercontent.com/wangsicong120/Emby/main/connectionmanager.js
 rm -f /opt/emby-server/system/dashboard-ui/embypremiere/embypremiere.js
-wget -P /opt/emby-server/system/dashboard-ui/embypremiere/ https://raw.githubusercontent.com/wangsicong120/Emby/main/embypremiere.js?token=GHSAT0AAAAAABSWSGH2USO2OHGSFSL3UA6KYRZZZ2A
+wget -P /opt/emby-server/system/dashboard-ui/embypremiere/ https://raw.githubusercontent.com/wangsicong120/Emby/main/embypremiere.js
 echo "web破解完成..."
 rm -f /opt/emby-server/system/Emby.Server.Implementations.dll
 wget -P /opt/emby-server/system/ https://github.com/wangsicong120/Emby/raw/main/Emby.Server.Implementations.dll
