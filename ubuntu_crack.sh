edc0540d24c41703dbaf20cfcabf61041ab38cfa
WINNIC <62343653+wangsicong120@users.noreply.github.com>
2022-03-23 18:22:55 +0800

    Update ubuntu_crack.sh

--- ubuntu_crack.sh
+++ ubuntu_crack.sh
@@ -1,16 +1,16 @@
 echo "Emby4.6.7版本"
 echo "-------------------web端破解-------------------"
 rm -f /opt/emby-server/system/Emby.Web.dll
-wget -P https://github.com/wangsicong120/Emby/blob/main/Emby.Web.dll /opt/emby-server/system/
+wget -O https://github.com/wangsicong120/Emby/blob/main/Emby.Web.dll /opt/emby-server/system/
 rm -f /opt/emby-server/system/MediaBrowser.Model.dll
-wget -P https://github.com/wangsicong120/Emby/raw/main/MediaBrowser.Model.dll /opt/emby-server/system/
+wget -O https://github.com/wangsicong120/Emby/raw/main/MediaBrowser.Model.dll /opt/emby-server/system/
 echo "核心破解完成..."
 rm -f /opt/emby-server/system/dashboard-ui/bower_components/emby-apiclient/connectionmanager.js
-wget -P https://raw.githubusercontent.com/wangsicong120/Emby/main/connectionmanager.js /opt/emby-server/system/dashboard-ui/bower_components/emby-apiclient/
+wget -O https://raw.githubusercontent.com/wangsicong120/Emby/main/connectionmanager.js /opt/emby-server/system/dashboard-ui/bower_components/emby-apiclient/
 rm -f /opt/emby-server/system/dashboard-ui/embypremiere/embypremiere.js
-wget -P https://raw.githubusercontent.com/wangsicong120/Emby/main/embypremiere.js /opt/emby-server/system/dashboard-ui/embypremiere/
+wget -O https://raw.githubusercontent.com/wangsicong120/Emby/main/embypremiere.js /opt/emby-server/system/dashboard-ui/embypremiere/
 echo "web破解完成..."
 rm -f /opt/emby-server/system/Emby.Server.Implementations.dll
-wget -P https://github.com/wangsicong120/Emby/raw/main/Emby.Server.Implementations.dll /opt/emby-server/system/
+wget -O https://github.com/wangsicong120/Emby/raw/main/Emby.Server.Implementations.dll /opt/emby-server/system/
 echo "Implementations替换认证..."
 echo "完成..."
